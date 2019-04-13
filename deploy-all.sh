46cdcd8895a0e2cb3a4795b3c11302db9353406e
shiming.liu <shiming.liu@bkjk.com>
2019-04-13 11:18:49 +0800

    init project

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,23 +1,8 @@
 #!/usr/bin/env bash
 
-# 注意顺序
-#
-# 1. common
-# 2. business
-# 3. 其它
-# 4. eureka
-# 5. openfeign
-# 6. monitor
-# 7. web
-# 8. dts
 
 cd summerframework-common        && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-business      && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-configcenter  && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-es            && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-jobcenter     && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-jpa           && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-mapping       && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-mybatis       && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-rabbit        && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-redis         && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
@@ -25,5 +10,3 @@ cd summerframework-webapi        && mvn clean deploy -DskipTests -Dmaven.javadoc
 cd summerframework-eureka        && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-openfeign     && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-monitor       && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-web           && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-dts           && mvn clean deploy -DskipTests -Dmaven.javadoc.skip=true && cd ..
--- install-all.sh
+++ install-all.sh
@@ -1,23 +1,8 @@
 #!/usr/bin/env bash
 
-# 注意顺序
-#
-# 1. common
-# 2. business
-# 3. 其它
-# 4. eureka
-# 5. openfeign
-# 6. monitor
-# 7. web
-# 8. dts
 
 cd summerframework-common        && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-business      && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-configcenter  && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-es            && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-jobcenter     && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-jpa           && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-mapping       && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-mybatis       && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-rabbit        && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-redis         && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
@@ -25,5 +10,3 @@ cd summerframework-webapi        && mvn install -DskipTests -Dmaven.javadoc.skip
 cd summerframework-eureka        && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-openfeign     && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
 cd summerframework-monitor       && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-web           && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
-cd summerframework-dts           && mvn install -DskipTests -Dmaven.javadoc.skip=true && cd ..
